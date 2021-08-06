729141b641691b1b1f253b3a5f7fc8a145029273
Sanya Boroda4 <sorokinalex1992@gmail.com>
2021-08-06 11:38:01 +0300

    Amended

--- Lesson 5/HM_01.py
+++ Lesson 5/HM_01.py
@@ -7,7 +7,7 @@ if rows.isdigit():
 
             for j in range(1, i+1):
                 j = str(j)      # перед тем как наполнять пустой массив элементами int, преобразовал в str назад.
-                matrix.append(j)
+                matrix.append(j+" ")
 
             new_matrix = matrix + matrix[-2::-1]
             pyramide = "".join(new_matrix)
--- Other/LIST_09.py
+++ Other/LIST_09.py
@@ -1,7 +1,7 @@
 matrix_2d = [[1, 3, 2],
              [3, 2, 4],
              [3, 5, 1]
-]
+             ]
 for i in matrix_2d:
     for j in i:
         print(j, end=" ")
