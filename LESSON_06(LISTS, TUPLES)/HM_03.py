5c838a6414c645ef1b60ca40203c4102497a4e10
Sanya Boroda4 <sorokinalex1992@gmail.com>
2021-08-09 15:38:42 +0300

    HM_03 amended

--- LESSON_06(LISTS, TUPLES)/HM_03.py
+++ LESSON_06(LISTS, TUPLES)/HM_03.py
@@ -6,4 +6,4 @@ k = int(input("Please enter element quantity for matrix: "))
 matrix_01 = [random.randint(0, 5) for i in range(k)]
 matrix_02 = [random.randint(0, 5) for j in range(k)]
 
-unique_elements = len(matrix_02 + matrix_01) - len(set(matrix_01 + matrix_02))
+unique_elements = len(set(matrix_01 + matrix_02))
