e18b8e0f51ec4adcda39ec4cb8892248c1cca0b6
Sanya Boroda4 <sorokinalex1992@gmail.com>
2021-08-10 11:08:35 +0300

    Amendments

--- LESSON_06(LISTS, TUPLES)/HM_02.py
+++ LESSON_06(LISTS, TUPLES)/HM_02.py
@@ -5,7 +5,7 @@ matrix = [random.randrange(100, 200, 10) for i in range(10)]
 k = int(input("Enter index number from 0 to 9: "))
 C = int(input("Enter a natural number between 100 & 200 multiple of 10: "))
 
-if k > 10 or k < 0:
+if k not in range(0, 11):
     print("You have entered invalid index. Restart the program.")
 else:
     if (C > 200 or C < 100) and C % 10 != 0 :
---
+++ LESSON_06(LISTS, TUPLES)/test.py
@@ -0,0 +1,2 @@
+a = [i for i in range(10)]
+print(a)
